56284c7aba775d7c095d4156fee462ff9c53cf67
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-03-05 13:39:38 -0600

    array with less than 5 numbers

---
+++ practice_one/filter_small_numbers.rb
@@ -0,0 +1,9 @@
+#filter out any numbers greater than 5, given the array [4, 82, 16, 24, 47, 63, 22, 8, 52, 58]
+
+all_numbers = [4, 82, 16, 24, 47, 63, 22, 8, 52, 58]
+small_numbers = []
+all_numbers.each do |number|
+  if number <= 5
+    small_number << number
+  end
+end
