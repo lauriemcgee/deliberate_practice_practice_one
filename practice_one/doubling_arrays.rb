33a46881f829643a2f2f416bb6ab2f4f4e83a133
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-03-05 14:41:20 -0600

    filtering arrays practice

--- practice_one/doubling_arrays.rb
+++ practice_one/doubling_arrays.rb
@@ -1,1 +1,31 @@
+# Double an array of numbers
+
+single_numbers = [8, 12, 3, 24, 18, 16, 38, 74, 12, 33, 74, 16]
+
+p single_numbers.map { |number| number * 2 }
+
+p single_numbers.map! { |number| number * 2 }
+
+
+single_numbers.each do |number|
+  number = number * 2
+  puts number
+end
+
+single_numbers.each do |number|
+  puts number * 2
+end
+
+
 # Double every other number in an array of numbers
+
+single_numbers = [8, 12, 3, 24, 18, 16, 38, 74, 12, 33, 74, 16]
+index_number = 0
+single_numbers.each do |number|
+  if index_number.odd?
+    number = number * 2
+  end
+  index_number += 1
+end
+
+p single_numbers
--- practice_one/filtering_arrays.rb
+++ practice_one/filtering_arrays.rb
@@ -1,3 +1,62 @@
 # Filter a list of strings to only show strings beginning with the letter 'a'
+string_of_words = ["aligator", "crocodile", "eagle", "raisin", "skittles", "apple", "elephant", "lollipop"]
+string_of_words_with_a = []
+
+string_of_words.each do |word|
+  word = word.split(", ")
+  word.each do |array|
+    if array[0] == "a"
+      string_of_words_with_a << array.to_s
+    end
+  end
+end
+
+p string_of_words_with_a
+
+
+# Filter a list of strings to only show strings beginning with the letter 'e'
+string_of_words = ["aligator", "crocodile", "eagle", "raisin", "skittles", "apple", "elephant", "lollipop"]
+e_words = []
+string_of_words.each do |word|
+  word = word.split(", ")
+  word.each do |array|
+    if array[0] == "e"
+      e_words << array.to_s
+    end
+  end
+end
+
+p e_words
+
 
 # Filter a list of strings to only show strings that contain the letter 'a'
+string_of_words = ["aligator", "crocodile", "eagle", "raisin", "skittles", "apple", "elephant", "lollipop"]
+words_with_an_a = []
+
+string_of_words.each do |word|
+  word = word.split(", ")
+  word.each do |array|
+    if array.include?("a")
+      words_with_an_a << array.to_s
+    end
+  end
+end
+
+p words_with_an_a
+
+
+# Filter a list of strings to only show strings that contain the letter "i"
+string_of_words = ["aligator", "crocodile", "eagle", "raisin", "skittles", "apple", "elephant", "lollipop"]
+i_words = []
+
+string_of_words.each do |word|
+  word = word.split(", ")
+  word.each do |array|
+    if array.include?("i")
+      i_words << array.to_s
+    end
+  end
+end
+
+p i_words
+
